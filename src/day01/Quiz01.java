e18686532cecdd60d5997af9f094fa732c495557
soomin <sm@sm.com>
2024-02-05 17:31:02 +0900

    학원에서 의도에 맞게 수정

--- src/day01/Quiz01.java
+++ src/day01/Quiz01.java
@@ -5,10 +5,11 @@ public class Quiz01 {
 		System.out.println("=======================================");
 		System.out.println("이름\t나이\t전화번호\t\t회비");
 		System.out.println("=======================================");
-		System.out.println("홍길동\t\"15\"\t3672-1234\t\\20000");
+		// 홍길동		"15"	3672-1234	\20000
+		System.out.println("홍길동\t\""+15+"\"\t"+3672+"-"+1234+"\t\\"+20000);
 		//	System.out.println("홍길동-홍길동");
-		System.out.println("고길동\t\"15\"\t2238-1234\t\\30000");
-		System.out.println("김말이\t\"15\"\t1234-1234\t\\50000");
+		System.out.println("고길동\t\""+15+"\"\t"+2238+"-"+1234+"\t\\"+30000);
+		System.out.println("김말이\t\""+15+"\"\t"+1234+"-"+1234+"\t\\"+50000);
 		System.out.println("---------------------------------------");
 		System.out.println("총합계\t\t\t\t\\100000");
 		System.out.println("---------------------------------------");
